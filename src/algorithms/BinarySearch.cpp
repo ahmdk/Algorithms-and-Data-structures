4378abfcd14a0a762a17df914e2954cf2d12754d
Ahmed Khalifa <17039015+ahmdk@users.noreply.github.com>
2020-05-22 14:48:39 -0700

    update binary search

--- src/algorithms/BinarySearch.cpp
+++ src/algorithms/BinarySearch.cpp
@@ -2,24 +2,20 @@
 
 int binarySearch(int *arr, int val, int start, int end)
 {
-    if(start > end) return -1;
-    int mid = start + (end - start)/2;
-    if(arr[start] == val) return start;
-    if(arr[end] == val) return end;
-    if(arr[mid] == val) return mid;    
-
-    if(val > arr[mid])
+    int mid;
+    while(start <= end)
     {
-        return binarySearch(arr, val, mid+1, end);
-    } else {
-        return binarySearch(arr, val, start, mid);
-    }    
-    return -1;  // not found
+        mid = start + (end - start)/2;
+        if(arr[mid] == val){ return mid; }
+        if(arr[mid] > val) { end = mid - 1; continue; }
+        if(arr[mid] < val) { start = mid + 1; continue; }
+    }
+    return -1;    
 }
 
 int main()
 {
     int test[] = {8, 25, 32, 47, 59, 78, 83, 91, 100, 115, 190};
-    int found = binarySearch(test, 59 , 0, 10);
-    std::cout << found << std::endl;
+    int index_found = binarySearch(test, 59 , 0, 10);
+    std::cout << index_found << std::endl;
 }   
